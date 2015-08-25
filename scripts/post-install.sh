60118965163b1732f0b44ffae7fabb8290793d43
Daniel Grammatica <daniel.grammatica@bourne-grammar.lincs.sch.uk>
2015-08-25 08:28:11 +0100

    Removed CUTS

---
+++ scripts/post-install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Bourne Grammar Post install script
+# Daniel Grammatica | dan@t0xic.me
+#
+# Shell script to finish provisioning of Linux dual boot machines
+#
+# Usage: copy script to /var/lib/cobbler/triggers/install/post
+# chmod +x /var/lib/cobbler/triggers/install/post/post-install.sh
+
+
